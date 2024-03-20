b2bf9a77fd825f7917ca3e8eaf84667712324f3a
SterrenHeer <sterrenheer@gmail.com>
2024-03-20 16:33:07 +0300

    add test gallery section

---
+++ js/script.js
@@ -0,0 +1,117 @@
+function slider({container, wrapper, field, slide, indicatorsSelector, elementsQuantity = 1, duration = 0, rowGap = 0}) {
+    let slideIndex = 1,
+        offset = 0,
+        mobile = false,
+        timer = 0,
+        dots = [];
+    const slides = document.querySelectorAll(slide),
+        slider = document.querySelector(container),
+        slidesWrapper = document.querySelector(wrapper),
+        slidesField = document.querySelector(field),
+        mediaQuery = window.matchMedia('(max-width: 768px)');
+
+    let width = deleteNotDigits(window.getComputedStyle(slidesWrapper).width) / elementsQuantity + 'px';
+
+    let indicators = document.createElement('ol');
+    indicators.classList.add(indicatorsSelector);
+    slider.append(indicators);
+
+    slidesField.style.width = 100 * slides.length + slides.length / elementsQuantity + "%";  
+
+    slides.forEach((slide, index) => {
+        slide.style.width = width;
+        if (index != 0) {
+            slide.style.paddingLeft = rowGap + 'px';
+        }
+    });
+    if (mediaQuery.matches) {
+        mobile = true;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+        mobile ? dot.style.width = 100 / slides.length + "%" : dot.style.width = '';
+        if (i == 0) {
+            dot.classList.add('active');
+        } 
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width) * (slideTo - 1);
+            makeTimer(duration);
+            changeActivity();
+        });
+    });
+
+    window.addEventListener('resize', (e) => {
+        width = deleteNotDigits(window.getComputedStyle(slidesWrapper).width) / elementsQuantity + 'px';
+        slides.forEach((slide, index) => {
+            slide.style.width = width;
+            if (index != 0) {
+                slide.style.paddingLeft = rowGap + 'px';
+            }
+        });
+        mediaQuery.matches ? mobile = true : mobile = false;
+        let dots = document.querySelectorAll('.dot')
+        dots.forEach((dot) => {
+            mobile ? dot.style.width = 100 / slides.length + "%" : dot.style.width = '';
+        });
+        slideIndex = 1,
+        offset = 0,
+        changeActivity();
+    }); 
+
+    makeTimer(duration);
+
+    function changeActivity() {
+        slidesField.style.transform = `translateX(-${offset}px)`;
+        dots.forEach(dot => dot.classList.remove('active'));
+        dots[slideIndex-1].classList.add('active');
+    }
+
+    function makeTimer(duration){
+        if (duration == 0) {
+            return;
+        }
+        clearInterval(timer)
+        timer = setInterval(function(){
+            if (offset == deleteNotDigits(width) * (slides.length - 1)) {
+                offset = 0;
+            } else {
+                offset += deleteNotDigits(width);
+            }
+    
+            if (slideIndex == slides.length) {
+                slideIndex = 1;
+            } else {
+                slideIndex++;
+            }
+    
+            changeActivity();
+        },duration);
+      }
+
+    function deleteNotDigits(str) {
+        return +str.replace(/[^\d\.]/g, '');
+    }
+}
+
+slider({
+    container: '.gallery_slider',
+    wrapper: '.gallery_slider_wrapper',
+    field: '.gallery_slider_inner',
+    slide: '.gallery_slide',
+    indicatorsSelector: 'gallery_slider_indicators',
+    nextArrow: '.gallery_slider_next',
+    prevArrow: '.gallery_slider_prev',
+    elementsQuantity: 4,
+    duration: 5000,
+    rowGap: 10
+});
