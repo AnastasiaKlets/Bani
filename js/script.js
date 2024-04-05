520768290a270306f3ec08554ba82cc66c31131d
SterrenHeer <sterrenheer@gmail.com>
2024-04-05 09:26:30 +0300

    add some pages (#17)
    * add pages
    
    * add pages
    
    * add pages
    
    ---------
    
    Co-authored-by: AnastasiaKlets <58228069+AnastasiaKlets@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// ScrollTrigger.batch("section > div", {
-//     interval: 0.1,
-//     batchMax: 3,
-//     onEnter: (batch) =>
-//         gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
-//     onLeave: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
-//     onEnterBack: (batch) =>
-//         gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
-//     onLeaveBack: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
-// });
-
 function slider({container, wrapper, field, slide, indicatorsSelector, elementsPerPage = 1, elementsPerPageMobile = 1, duration = 0, rowGap = 0}) {
     let slideIndex = 1,
         offset = 0,
