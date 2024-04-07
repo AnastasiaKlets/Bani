1f739f784a17f5672193cb7a15b0204ebb124bb5
SterrenHeer <sterrenheer@gmail.com>
2024-04-07 16:57:41 +0300

    add product card page (#18)
    * add test page for product card
    
    * add tabs and image choice feature to product card page
    
    * add test media for product section

--- js/script.js
+++ js/script.js
@@ -186,6 +186,44 @@ function slider({container, wrapper, field, slide, indicatorsSelector, elementsP
     slidesField.addEventListener('touchend', end);
 }
 
+function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+    let tabs = document.querySelectorAll(tabsSelector),
+		tabsContent = document.querySelectorAll(tabsContentSelector),
+		tabsParent = document.querySelector(tabsParentSelector);
+
+	function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+	}
+
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(activeClass);
+    }
+    
+    hideTabContent();
+    showTabContent();
+
+	tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+	});
+}
+
 function openModal(modalSelector) {
     const modal = document.querySelector(modalSelector);
     modal.classList.add('show');
@@ -252,6 +290,8 @@ if (document.querySelector('.consult') != null) {
     modal('[data-modal]', 'data-close', '.consult');
 }
 
+tabs('.product_tab_item', '.product_tab_content', '.product_tab_header', 'product_tab_active');
+
 const menu = document.querySelectorAll('.catalog_menu'),
     menu_items = document.querySelectorAll('.catalog_menu-items'),
     menu_item = document.querySelectorAll('.catalog_menu-item');
@@ -272,3 +312,15 @@ menu_item.forEach(item => {
         item.classList.add('active');
     });
 });
+
+let bottom_images = document.querySelectorAll('.product_images_bottom img');
+
+bottom_images.forEach(image => {
+    image.addEventListener('click', (e) => {
+        let new_src = image.getAttribute('src');
+        let main_image = document.querySelector('.product_images_main img');
+        let old_src = main_image.getAttribute('src');
+        main_image.setAttribute('src', new_src)
+        image.setAttribute('src', old_src)
+    });
+}); 
